9f0dc167e14ee8088b89880f109b0698d1a11a48
Dmitry Morozov <dmitry.a.morozov@gmail.com>
2018-05-03 10:19:29 -0700

    List.lastTwo

--- src/main/kotlin/Lists.kt
+++ src/main/kotlin/Lists.kt
@@ -1,7 +1,17 @@
+package list
 
-fun<T> tryLast(xs: List<T>): T? =
+fun<T> last(xs: List<T>): T? =
     when (xs.size) {
         0 -> null
         1 -> xs[0]
-        else -> tryLast(xs.subList(1, xs.size))
+        else -> last(xs.subList(1, xs.size))
     }
+
+fun<T> lastTwo(xs: List<T>): Pair<T, T>? =
+    if (xs.size == 2)
+        Pair(xs[0], xs[1])
+    else if (xs.size > 2)
+        lastTwo( xs.drop(1))
+    else
+        null
+
--- src/test/kotlin/ListsTest.kt
+++ src/test/kotlin/ListsTest.kt
@@ -1,14 +1,20 @@
 
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ListsTest {
 
     @Test
-    fun simpleTestCase() {
-        assertEquals( "d",tryLast( listOf( "a", "b", "c", "d")))
-        assertEquals( null, tryLast( emptyList<String>()))
+    fun `last element of a list`() {
+        assertEquals( "d", list.last( listOf( "a", "b", "c", "d")))
+        assertEquals( null, list.last( emptyList<String>()))
+    }
+
+    @Test
+    fun `last and penultimate elements of a list`() {
+        assertEquals( "c" to "d", list.lastTwo ( listOf( "a", "b", "c", "d")))
+        assertEquals( null, list.lastTwo ( listOf( "a")))
     }
 }
